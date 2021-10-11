16c1e7a77f26903bee6700f1445489605a376be6
MaxxBurn <tarkaklavio@gmail.com>
2021-10-11 20:18:32 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Artstation
+{
+    public class Info
+    {
+        
+    }
+
+    public class ArtstationSorted
+    {
+        [JsonProperty("data")]
+        public List<Info> Array { get; set; }
+    }
+
+    class Backend
+    {
+        private readonly string url = "https://www.artstation.com/projects.json?sort_by=community";
+        
+        public async Task GetJson()
+        {
+            JObject jsonObject = new JObject(JObject.Parse(await Program.client.GetStringAsync(url)));
+
+            Console.WriteLine(jsonObject.ToString());
+        }
+    }
+
+    class Program
+    {
+        public static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Backend obj = new Backend();
+            await obj.GetJson();
+        }
+    }
+}
